e9fc38dd213372c7ab423f56025bb9b4e4d2a554
Shuichi Aizawa <sa396@columbia.edu>
2018-06-18 21:38:21 -0400

    code cleanup

--- main.js
+++ main.js
@@ -6,25 +6,6 @@ navigator.mediaDevices.getUserMedia({audio:true})
 	var audioContext, gainNode, rec=0, tracks=[];
 
 	tracks[0] = {};
-	tracks[0].audio = document.getElementById("track0");
-	tracks[0].audio.onended = function() {
-		if (rec) {
-			if (mediaRecorder.state == "inactive") {
-				gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
-				mediaRecorder.start();
-				tracks[rec].button.style.background = "red";
-			}
-			else if (mediaRecorder.state == "recording") {
-				mediaRecorder.stop();
-				gainNode.gain.setValueAtTime(1, audioContext.currentTime);
-			}
-		}
-
-		for (var i=4;i>=0;--i) {
-			if (tracks[i].audio.src) tracks[i].audio.play();
-		}
-	}
-
 	tracks[0].button = document.getElementById("button0");
 	tracks[0].button.onclick = function() {
 		if (!audioContext) {
@@ -52,14 +33,26 @@ navigator.mediaDevices.getUserMedia({audio:true})
 		}
 	}
 
-	var mediaRecorder = new MediaRecorder(stream);
-	mediaRecorder.ondataavailable = function(e) {
-		tracks[rec].audio.src = URL.createObjectURL(e.data);
-		tracks[rec].button.style.background = "";
-		rec = 0;
+	tracks[0].audio = document.getElementById("track0");
+	tracks[0].audio.onended = function() {
+		if (rec) {
+			if (recorder.state == "inactive") {
+				gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
+				recorder.start();
+				tracks[rec].button.style.background = "red";
+			}
+			else if (recorder.state == "recording") {
+				recorder.stop();
+				gainNode.gain.setValueAtTime(1, audioContext.currentTime);
+			}
+		}
+
+		for (var i=4;i>=0;--i) {
+			if (tracks[i].audio.src) tracks[i].audio.play();
+		}
 	}
 
-	for (var i=4;i>0;--i) {
+	for (var i=1;i<=4;++i) {
 		initTrack(i);
 	}
 
@@ -68,7 +61,7 @@ navigator.mediaDevices.getUserMedia({audio:true})
 		tracks[index].audio = document.getElementById("track"+index);
 		tracks[index].button = document.getElementById("button"+index);
 		tracks[index].button.onclick = function() {
-			if (mediaRecorder.state == "inactive") {
+			if (recorder.state == "inactive") {
 				if (rec) tracks[rec].button.style.background = "";
 				if (rec == index) {
 					rec = 0;
@@ -79,6 +72,13 @@ navigator.mediaDevices.getUserMedia({audio:true})
 				}
 			}
 		}
+	}
+
+	var recorder = new MediaRecorder(stream);
+	recorder.ondataavailable = function(e) {
+		tracks[rec].audio.src = URL.createObjectURL(e.data);
+		tracks[rec].button.style.background = "";
+		rec = 0;
 	}
 })
 .catch(function(e) {
